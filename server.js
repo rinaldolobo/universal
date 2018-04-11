07e080469b647bddbb73321dce38d102490ba50f
Rinaldo Lobo <r.lobo@murgency.com>
2018-04-11 16:18:33 +0530

    Integration with abstract class

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/*', (req, res) => {
       const obj = new fabric.PathGroup(objects, options);
       dom.window.document.querySelector("svg").outerHTML = '<img src="' + canvas.toDataURL() + '" />';
       canvas.add(obj);
-      res.send(dom.window.document);
+      res.send(dom.window.document.querySelector(".ai-abstract-main").outerHTML);
     });
   });
 });
